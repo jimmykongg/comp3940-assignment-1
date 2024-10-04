5fb1b1b5b9384eb17929ff91751c5ca02895613a
Jimmy Kong <jimmyatwork368@gmail.com>
2024-10-03 21:32:01 -0700

    bugfix: Fix websocket connection issue (quizRoom.js, quizRoomSocket.java)

--- src/main/java/com/quizapp/quizroom/quizRoomSocket.java
+++ src/main/java/com/quizapp/quizroom/quizRoomSocket.java
@@ -1,8 +1,8 @@
 package com.quizapp.quizroom;
 
 
-import javax.websocket.*;
-import javax.websocket.server.ServerEndpoint;
+import jakarta.websocket.*;
+import jakarta.websocket.server.ServerEndpoint;
 import java.io.IOException;
 
 @ServerEndpoint("/quizRoomSocket")
