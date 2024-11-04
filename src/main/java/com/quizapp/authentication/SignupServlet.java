9732892ff9eed058960f354f603a7d7f76db4052
Steven Ly <steven.ly236@gmail.com>
2024-11-04 14:27:09 -0800

    test push

--- src/main/java/com/quizapp/authentication/SignupServlet.java
+++ src/main/java/com/quizapp/authentication/SignupServlet.java
@@ -26,7 +26,7 @@ public class SignupServlet extends HttpServlet {
         try {
             con = DatabaseConnection.initializeDatabase();
 
-            if (isUserNotExisted(username, con, res)) {
+            if (isUserNotExisted(usernamee, con, res)) {
                 insertUser(username, password, con, res);
                 HttpSession session = req.getSession();
                 session.setAttribute("username", username);
