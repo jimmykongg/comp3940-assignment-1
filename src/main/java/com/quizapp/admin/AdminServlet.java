9efd8c7994ead1f94013c1d474a9604cd23bca23
Victor Wong <victor_wong444@hotmail.com>
2024-11-06 19:57:42 -0800

    pipeline test for error

--- src/main/java/com/quizapp/admin/AdminServlet.java
+++ src/main/java/com/quizapp/admin/AdminServlet.java
@@ -8,7 +8,7 @@ import com.quizapp.database.DatabaseConnection;
 
 
 @WebServlet("/admin")
-public class AdminServlet extends HttpServlet {
+public class AdinServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res){
 
